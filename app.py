eed49439bc9e5ee2088f8fefedbe01486b7ace97
Coderaftsman <155223503+Coderaftsman@users.noreply.github.com>
2026-02-06 02:48:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import os
 
 app = Flask(__name__)
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FILE_PATH = os.path.join(BASE_DIR, "files", "forensics_easy_cyber_matrix.png")
+
 
 @app.route("/")
 def index():
@@ -11,11 +14,10 @@ def index():
 
 @app.route("/download")
 def download():
-    return send_file("C:\Users\Welcome\Desktop\forensics_easy\files\forensics_easy_image (1).png", as_attachment=True)
+    return send_file(FILE_PATH, as_attachment=True)
 
 
 # Local + Nimbus compatible
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-
     app.run(host="0.0.0.0", port=port, debug=True)

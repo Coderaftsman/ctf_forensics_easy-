190aec79b088ce8a350f8fa43a2b8db69db2610a
Coderaftsman <155223503+Coderaftsman@users.noreply.github.com>
2026-02-06 03:27:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ import os
 
 app = Flask(__name__)
 
+# Get project base directory
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-FILE_PATH = os.path.join(BASE_DIR, "files", "forensics_easy_cyber_matrix.png")
+
+# Correct file path inside project
+FILE_PATH = os.path.join(BASE_DIR, "files", "forensics_easy_image.png")
 
 
 @app.route("/")
@@ -14,10 +17,11 @@ def index():
 
 @app.route("/download")
 def download():
+    # This is where send_file must be used
     return send_file(FILE_PATH, as_attachment=True)
 
 
-# Local + Nimbus compatible
+# Run locally (Nimbus will ignore this and use gunicorn)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
